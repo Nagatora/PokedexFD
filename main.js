5ee64281dea8236dc3bb0e7b3104f97ac65a8814
Nagatora <99711842+Nagatora@users.noreply.github.com>
2022-06-13 09:52:43 -0300

    Second Commit

--- main.js
+++ main.js
@@ -1,25 +1,45 @@
-const btnBuscar = document.getElementById('btnBuscar')
+const imgPokemon = document.getElementsByClassName('imgPokemon');
+const statsPokemon = document.getElementsByClassName('statsPokemon');
+const buttonSearch = document.getElementById('buttonSearch')
 
-function mostrarPokemon(pokemon){
-    const imgPokemon = document.getElementsByClassName('imgPokemon');
-    const sprite = document.createElement('img');
-    sprite.src = pokemon.sprites.front_default;
-    imgPokemon.appendChild(sprite);
+
+buttonSearch.addEventListener('click', getPokemon);
+
+function getPokemon() {
+    const nameOrID = document.getElementById('nameOrID').value.toLowerCase();
+    fetchPokemon(nameOrID);
 }
 
-function fetchPokemon(nombreOrID){
-    fetch(`https://pokeapi.co/api/v2/pokemon/${nombreOrID}/`)
-    .then((res) => res.json())
-    .then((pokemon) =>  {
-        const imgPokemon = document.getElementsByClassName('imgPokemon');
-        const sprite = document.createElement('img');
-        sprite.src = pokemon.sprites.front_default
-        imgPokemon.appendChild(sprite);
+function fetchPokemon(nameOrID){
+    fetch(`https://pokeapi.co/api/v2/pokemon/${nameOrID}/`)
+    .then((res) => {
+        if (res.status === 404) {
+            alert('This pokemon is not available. Try another one!')
+        }   else{
+            return res.json()
+        }
+    })
+    .then(resJSON => {
+        const allItems = []
 
-    });
+        const result = []
+    
+    for(let pokemonInfo in resJSON){
+        result.push([pokemonInfo, resJSON[pokemonInfo]])
+    }
+    console.table(result)
+
+    const imgContainer = document.createElement('img')
+    imgContainer.src = result[14][1].front_default
+
+    const pokemonName = document.createElement('h2');
+    pokemonName.innerText = `Name: ${result[10][1]} - ID${result[6][1]}`
+
+    const pokemonType = document.createElement('h2')
+    pokemonType.innerText = `Type: ${result[16][1][0].type.name}`
+
+    imgPokemon.append(imgContainer)
+    statsPokemon.append(pokemonName, pokemonType)
+})
 }
 
-btnBuscar.addEventListener('click', () =>{
-    const nombreOrID = document.getElementById('nombreOrID').value;
-    fetchPokemon(nombreOrID);
-});
